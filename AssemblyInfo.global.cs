46cf43bb14a61b9e54475c160f3c1279c164a192
Stephan Volmer <stv@zuehlke.com>
2014-08-14 10:46:28 +0200

    Added support for publishing NuGet package

--- AssemblyInfo.global.cs
+++ AssemblyInfo.global.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NDiagnostics.Metering")]
 [assembly: AssemblyCopyright("Copyright © Stephan Volmer 2014")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
