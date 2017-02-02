5d6b1846b7871847b87cfa7b9eabb052607edbcb
Stephan Volmer <stv@zuehlke.com>
2017-02-02 15:39:15 +0100

    Upgraded to version 1.0.0

--- AssemblyInfo.global.cs
+++ AssemblyInfo.global.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NDiagnostics.Metering")]
-[assembly: AssemblyCopyright("Copyright © Stephan Volmer 2015")]
-[assembly: AssemblyVersion("0.9.8.0")]
-[assembly: AssemblyFileVersion("0.9.8")]
+[assembly: AssemblyCopyright("Copyright © Stephan Volmer 2017")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
--- license.md
+++ license.md
@@ -1,4 +1,4 @@
-Copyright (c) 2014, Stephan Volmer
+Copyright (c) 2017, Stephan Volmer
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
